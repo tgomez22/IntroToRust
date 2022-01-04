5c88f9fccea5420699fd6d9e795614f12584eb1a
Tristan Gomez <gomez22@pdx.edu>
2022-01-04 14:12:41 -0800

    Finished initial documentation and readme

---
+++ modexp/README.md
@@ -0,0 +1,24 @@
+## Tristan Gomez - Winter 2022
+
+# Project Name: Modular Exponentiation 
+
+### What I did
+For this assignment I wrote a command line modular exponentiation tool. The tool takes three non-negative, positive integers as command line arguments. The tool attempts to convert the arguments from strings into u64. If the conversion fails due to some bad input then both specific and generic error messages are displayed to the user. If the user provides too many or too few arguments then both specific and generic error messages are displayed to the user. The generic error is a usage message for the program, but the specific error message describes why the error occurred. Assuming all inputs are valid then the modular exponentiation is performed and the result of the computation is displayed to the user and the program exits.
+
+
+### How it went
+I found this homework assignment to be fairly challenging but fun. The C-like syntax made it easy to jump into this language but it also got me in trouble in some spots. For example, I initially forgot to add the `mut` keyword to several of my variables which resulted in compiler errors. 
+
+Speaking of compiler errors, I have found that Rust's compiler errors are quite a bit nicer/more helpful when it comes to debugging. Generally the compiler provides good information on:
+    * What the error is exactly
+    * What exactly can be done to remedy the error.
+
+These compiler errors occurred most often: when I was attempting to compare different data types, when I would forget to make a variable mutable, when I would accidentally make a variable mutable that never changed its value, and a few times when I wrote a potentially infinite loop. The errors were initially difficult to figure out since they were new types of error messages, but ultimately they were very helpful and much more descriptive than C based error messages.
+
+I really enjoy the `match` syntax and control flow. It was a little difficult to get a hang of initially. I struggled with when to use Ordering with match. Match is pretty intuitive and I found it interesting that I got a lot of compiler warnings initially when I wrote if-else-else if statements. The errors *strongly* suggested that I use match instead. 
+
+### How I tested my work
+
+I used the given assertions in the assignment sheet, as well as, wrote a few of my own test cases to test my program programatically. I also ran my own manual tests on the program using negative values and characters. The generic usage error displayed during each trial of invalid input. I changed the given `parsenum()` function to display the specific `ParseIntError` that resulted during the attempted conversion so that the user would get specific feedback as to why their input was invalid. 
+
+I attempted to write test cases calling just `parsenum()` while passing in invalid inputs. For example, I attempted to pass in -5 as an argument and then assert that -5 would not equal the return value of `parsenum()`. The compiler would not compile after I wrote this test because -5 is not a valid value for a u64. I realized that if a parsing error occurs in this function then it doesn't return, but rather it calls `error()` and exits. I also attempted to write test cases passing invalid input directly into `modexp()`, but again I was prevented from doing so by the compiler because the values were not valid for a u64. I found this feature to be helpful and reassuring; however, It also frustrated me a bit. I feel like I should be able to provide invalid input in the confines of a test. Perhaps I am not well-versed enough to know how to implement tests like this in the proper rust way. I considered rewriting most functions to return a Result instead of a u64 because I could most likely test the Err value, but I felt that may be beyond the scope of this first assignment. Edge cases and invalid inputs all seem to be handled correctly right now, and all my tests pass.
--- modexp/src/main.rs
+++ modexp/src/main.rs
@@ -1,33 +1,48 @@
-//! Command-line modular exponentation tool
-//! 
+//! Command-line modular exponentiation tool
+//!
 //! Tristan Gomez 2022
 
-use std::env;
+/// Lib inclusions.
+/// Ordering for use in match control flow.
+/// Env for use in obtaining command line args.
 use std::cmp::Ordering;
+use std::env;
 
-
+/// This method performs an initial match on the number of given command line args.
+/// If there are too few or too many arguments given then a specific error message is given to help the user.
+/// A generic usage error message is also displayed.
+///
+/// If the proper number of arguments is given then the arguments are parsed from strings into u64. If the conversion fails
+/// a specific error message for why the conversion failed is displayed to the user, as well as, a generic usage error message.
+///
+/// If the arguments are successfully parsed then modexp() is called and the parsed arguments are passed in. Once modexp returns,
+/// display its return value to the user and exit the program successfully.
 fn main() {
-    
+    // Gather command line arguments to be stored in a vector of strings.
     let args: Vec<String> = env::args().collect();
 
     // Check if there is the expected number of arguments. Call error() if incorrect number of arguments. Else call modexp().
     match args.len().cmp(&4) {
         Ordering::Less => {
-            println!("Too few arguments. This program takes 3 arguments and {} were provided", args.len() - 1);
+            println!(
+                "Too few arguments. This program takes 3 arguments and {} were provided",
+                args.len() - 1
+            );
             error()
-        },
+        }
         Ordering::Greater => {
-            println!("Too many arguments. This program takes 3 arguments and {} were provided", args.len() -1);
+            println!(
+                "Too many arguments. This program takes 3 arguments and {} were provided",
+                args.len() - 1
+            );
             error()
-        },
+        }
         Ordering::Equal => {
-
             // Found expected number of arguments. Convert them from String to u64 using parsenum(), and store result in vector.
-            let converted_args: Vec<u64> = vec![
-                parsenum(&args[1]),
-                parsenum(&args[2]),
-                parsenum(&args[3])
-            ];
+            // If any inputs fail to parse successfully then display both a specific err message with exact type of error, as well as,
+            // a generic usage error.
+            let converted_args: Vec<u64> =
+                vec![parsenum(&args[1]), parsenum(&args[2]), parsenum(&args[3])];
 
             let ret_val = modexp(converted_args[0], converted_args[1], converted_args[2]);
             println!("{}", ret_val);
@@ -36,27 +51,41 @@ fn main() {
     }
 }
 
-
-fn modexp(x: u64, y: u64, m: u64) -> u64{
+/// This method performs the modular exponentiation. It takes 3, u64 arguments and returns a u64
+/// upon success. If there is some sort of error then specific and generic error messages are displayed to the user.
+fn modexp(x: u64, y: u64, m: u64) -> u64 {
     let mut y = y;
     let m: u128 = u128::from(m);
     let mut x: u128 = u128::from(x);
 
     match m {
-        0 => error(),
+        0 => {
+            println!(
+                "The third argument cannot be 0. It must be a positive integer greater than 0."
+            );
+            error();
+        }
         1 => 0,
         _ => {
-            let mut ret_val: u128 = 1;   
-            while  y > 0 {
+            let mut ret_val: u128 = 1;
+            while y > 0 {
                 if y % 2 == 1 {
                     ret_val = (ret_val * x) % m;
                 }
                 y /= 2;
                 x = (u128::pow(x, 2)) % m;
             }
-            u64::try_from(ret_val).unwrap()
+
+            // Convert the ret_val into a u64 and return it
+            // or
+            // Somehow the converstion from u128 to u64 failed even though the return value
+            // should be <= m, which fits into a u64.
+            u64::try_from(ret_val).unwrap_or_else(|caught_err| {
+                eprintln!("{}", caught_err);
+                error()
+            })
         }
-    } 
+    }
 }
 
 /// Print a usage error message and exit.
@@ -66,53 +95,59 @@ fn error() -> ! {
 }
 
 /// Parse the given string as a `u64`
-fn parsenum(s: &str) -> u64{
-
+fn parsenum(s: &str) -> u64 {
     // I changed the provided code to display the ParseIntError given if the conversion
     // fails, so that way users can have more helpful error messages rather than the standard
     // error message.
-    s.parse().unwrap_or_else(|err_res| {
-            println!("{}", err_res);
-            error()
-        }
-    )
+    s.parse().unwrap_or_else(|ret_val| {
+        eprintln!("{}", ret_val);
+        error()
+    })
 }
 
-
-/// Test cases
+/// Test cases below
 #[test]
-fn test_modexp1(){
+fn test_modexp1() {
     // Largest prime less than 2**64
-
     let bigm = u64::max_value() - 58;
     assert_eq!(0, modexp(bigm - 2, bigm - 1, 1));
-
-    assert_eq!(16, modexp(2, 20, 17));
 }
 
 #[test]
-fn test_modexp2(){
+fn test_modexp2() {
     let bigm = u64::max_value() - 58;
     assert_eq!(1, modexp(bigm - 2, bigm - 1, bigm));
 }
 
 #[test]
-fn test_modexp3(){
+fn test_modexp3() {
     let bigm = u64::max_value() - 58;
     assert_eq!(827419628471527655, modexp(bigm - 2, (1 << 32) + 1, bigm));
 }
 
 #[test]
-fn test_modexp4(){
+fn test_modexp4() {
     assert_eq!(4, modexp(10, 9, 6));
 }
 
 #[test]
-fn test_modexp5(){ 
+fn test_modexp5() {
     assert_eq!(34, modexp(450, 768, 517));
 }
 
 #[test]
-fn test_modexp6(){
+fn test_modexp6() {
     assert_eq!(16, modexp(2, 20, 17));
+}
+
+#[test]
+fn test_modexp7() {
+    let num = u64::max_value();
+    assert_eq!(166, modexp(num, num, 517));
+}
+
+#[test]
+fn test_modexp8() {
+    let num = u64::max_value();
+    assert_eq!(0, modexp(num, num, num));
 }
